3122cc38f8b7b5d48ad965471fec28effc14c63b
Shea Newkirk <longbeachwebdesign@gmail.com>
2020-10-12 13:49:10 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
         <div class="footer-widgets"><?php dynamic_sidebar( 'footer-widgets' ); ?></div>
 		<div class="site-info">
             &copy; <?php echo date('Y'); ?> <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a><br>
-			<span>Designed by <a href="http://www.longbeachwebdesign.com" title="Long Beach, CA Website Design, Ecommerce, Website Hosting, & Graphics" target="_blank">LBWD</a></span>
+			<span>Designed by <a href="http://www.longbeachwebdesign.com" alt="Long Beach California ca web design website ecommerce e-commerce hosting graphic" title="Long Beach, CA Website Design, Ecommerce, Website Hosting, & Graphics" target="_blank">LBWD</a></span>
 
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
